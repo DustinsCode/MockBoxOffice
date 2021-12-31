ff9c9ce95b1fad3740512e1460152f8f68083665
Dustin Thurston <dthurston1996@gmail.com>
2021-12-31 02:10:25 -0500

    started working on profile buttons

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,29 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
+  const profiles = [
+    {id: 1,name: 'Dustin'},
+    {id: 2,name: 'Mom'},
+    {id: 3,name: 'Dad'},
+    {id: 4,name: 'Friends'}
+  ]
+   
   return (
     <div className={styles.header}>
       <span className={styles.logo}>HBO Max</span>
       <div>Who is watching?</div>
+      <div className={styles.profileBox}>
+        {
+          profiles.map((profile) => {
+            return (
+              <div className={styles.profileButton} key={profile.id}>
+                <div className={styles.letter}>{profile.name.charAt(0)}</div>
+                {profile.name}
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
